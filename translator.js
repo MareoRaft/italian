4a55e8f055f236192727bea551e153ddcdb1ad04
Matthew Lancellotti <mareoraft@gmail.com>
2017-12-09 23:44:28 -0500

    fixed a glitch preventing translator from working.

--- translator.js
+++ translator.js
@@ -1,130 +1,74 @@
+var inputToOutput = {
+	'avere': 'have',
+	'have': 'avere',
+	'abitare': 'live',
+	'live': 'abitare',
+	'andare': 'go',
+	'go': 'andare',
+	'arrivare': 'arrive',
+	'arrive': 'arrivare',
+	'ballare': 'dance',
+	'dance': 'ballare',
+	'cantare': 'sing',
+	'sing': 'cantare',
+	'cominciare': 'begin',
+	'begin': 'cominciare',
+	'comprare': 'buy',
+	'buy': 'comprare',
+	'dare': 'give',
+	'give': 'dare',
+	'dimenticare': 'forget',
+	'forget': 'dimenticare',
+	'dire': 'talk',
+	'talk': 'dire',
+	'dovere': 'have to',
+	'have to': 'dovere',
+	'essere': 'be',
+	'be': 'essere',
+	'fare': 'do',
+	'do': 'fare',
+	'frequentare': 'attend',
+	'attend': 'frequentare',
+	'guidare': 'drive',
+	'drive': 'guidare',
+	'guardare': 'watch',
+	'watch': 'guardare',
+	'imparare': 'learn',
+	'learn': 'imparare',
+	'insegnare': 'teach',
+	'teach': 'insegnare',
+	'lavorare': 'work',
+	'work': 'lavorare',
+	'mangiare': 'eat',
+	'eat': 'mangiare',
+	'parlare': 'talk',
+	'talk': 'parlare',
+	'potere': 'be able to',
+	'be able to': 'potere',
+	'ricordare': 'remember',
+	'remember': 'ricordare',
+	'sapere': 'know a fact',
+	'know a fact': 'sapere',
+	'spiegare': 'explain',
+	'explain': 'spiegare',
+	'studiare': 'study',
+	'study': 'studiare',
+	'uscire': 'go out',
+	'go out': 'uscire',
+	'venire': 'come',
+	'come': 'venire',
+	'volere': 'want',
+	'want': 'volere',
+}
 
-function translate(){
-word = trans.a.value
-
-if(word=='avere')
-{trans.answerOne.value = 'have'}
-
-else if(word=='have')
-{trans.answerOne.value = 'avere'}
-else if(word=='abitare')
-{trans.answerOne.value = 'live'}
-else if(word=='live')
-{trans.answerOne.value = 'abitare'}
-else if(word=='andare')
-{trans.answerOne.value = 'go'}
-else if(word=='go')
-{trans.answerOne.value = 'andare'}
-else if(word=='arrivare')
-{trans.answerOne.value = 'arrive'}
-else if(word=='arrive')
-{trans.answerOne.value = 'arrivare'}
-else if(word=='ballare')
-{trans.answerOne.value = 'dance'}
-else if(word=='dance')
-{trans.answerOne.value = 'ballare'}
-else if(word=='cantare')
-{trans.answerOne.value = 'sing'}
-else if(word=='sing')
-{trans.answerOne.value = 'cantare'}
-else if(word=='cominciare')
-{trans.answerOne.value = 'begin'}
-else if(word=='begin')
-{trans.answerOne.value = 'cominciare'}
-else if(word=='comprare')
-{trans.answerOne.value = 'buy'}
-else if(word=='buy')
-{trans.answerOne.value = 'comprare'}
-else if(word=='dare')
-{trans.answerOne.value = 'give'}
-else if(word=='give')
-{trans.answerOne.value = 'dare'}
-else if(word=='dimenticare')
-{trans.answerOne.value = 'forget'}
-else if(word=='forget')
-{trans.answerOne.value = 'dimenticare'}
-else if(word=='dire')
-{trans.answerOne.value = 'talk'}
-else if(word=='talk')
-{trans.answerOne.value = 'dire'}
-else if(word=='dovere')
-{trans.answerOne.value = 'have to'}
-else if(word=='have to')
-{trans.answerOne.value = 'dovere'}
-else if(word=='essere')
-{trans.answerOne.value = 'be'}
-else if(word=='be')
-{trans.answerOne.value = 'essere'}
-else if(word=='fare')
-{trans.answerOne.value = 'do'}
-else if(word=='do')
-{trans.answerOne.value = 'fare'}
-else if(word=='frequentare')
-{trans.answerOne.value = 'attend'}
-else if(word=='attend')
-{trans.answerOne.value = 'frequentare'}
-else if(word=='guidare')
-{trans.answerOne.value = 'drive'}
-else if(word=='drive')
-{trans.answerOne.value = 'guidare'}
-else if(word=='guardare')
-{trans.answerOne.value = 'watch'}
-else if(word=='watch')
-{trans.answerOne.value = 'guardare'}
-else if(word=='imparare')
-{trans.answerOne.value = 'learn'}
-else if(word=='learn')
-{trans.answerOne.value = 'imparare'}
-else if(word=='insegnare')
-{trans.answerOne.value = 'teach'}
-else if(word=='teach')
-{trans.answerOne.value = 'insegnare'}
-else if(word=='lavorare')
-{trans.answerOne.value = 'work'}
-else if(word=='work')
-{trans.answerOne.value = 'lavorare'}
-else if(word=='mangiare')
-{trans.answerOne.value = 'eat'}
-else if(word=='eat')
-{trans.answerOne.value = 'mangiare'}
-else if(word=='parlare')
-{trans.answerOne.value = 'talk'}
-else if(word=='talk')
-{trans.answerOne.value = 'parlare'}
-else if(word=='potere')
-{trans.answerOne.value = 'be able to'}
-else if(word=='be able to')
-{trans.answerOne.value = 'potere'}
-else if(word=='ricordare')
-{trans.answerOne.value = 'remember'}
-else if(word=='remember')
-{trans.answerOne.value = 'ricordare'}
-else if(word=='sapere')
-{trans.answerOne.value = 'know a fact'}
-else if(word=='know a fact')
-{trans.answerOne.value = 'sapere'}
-else if(word=='spiegare')
-{trans.answerOne.value = 'explain'}
-else if(word=='explain')
-{trans.answerOne.value = 'spiegare'}
-else if(word=='studiare')
-{trans.answerOne.value = 'study'}
-else if(word=='study')
-{trans.answerOne.value = 'studiare'}
-else if(word=='uscire')
-{trans.answerOne.value = 'go out'}
-else if(word=='go out')
-{trans.answerOne.value = 'uscire'}
-else if(word=='venire')
-{trans.answerOne.value = 'come'}
-else if(word=='come')
-{trans.answerOne.value = 'venire'}
-else if(word=='volere')
-{trans.answerOne.value = 'want'}
-else if(word=='want')
-{trans.answerOne.value = 'volere'}
-
-
-else{
-trans.answerOne.value = 'no translation'}
+function translateword(){
+	var input = trans.word.value
+	var output = undefined
+	if (input in inputToOutput) {
+		output = inputToOutput[input]
+	}
+	else {
+		output = 'word not found'
+	}
+	trans.answer.value = output
 }
