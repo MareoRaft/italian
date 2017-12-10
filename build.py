78770744595ad77e2c5c52baf86a7cee6e191398
Matthew Lancellotti <mareoraft@gmail.com>
2017-12-09 23:02:54 -0500

    incorporated vocab in with others.  deleted index.html.

--- build.py
+++ build.py
@@ -3,29 +3,37 @@ from os import mkdir
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape, Markup # jinja2: see http://jinja.pocoo.org/docs/2.10/api/ and http://jinja.pocoo.org/docs/2.10/templates/#extends
 
+BUILD_DIR = '_build'
 
-banner_snippet = Markup("<a href='index.html'>index</a> | <a href='1.html'>italian 1</a> | <a href='2.html'>italian 2</a> | <a href='3.html'>italian 3</a> | <a href='vocab.html'>italian vocabulary</a>")
-course_num_to_info = {
-	1: {
+banner_snippet = Markup("<a href='1.html'>italian 1</a> | <a href='2.html'>italian 2</a> | <a href='3.html'>italian 3</a> | <a href='vocab.html'>italian vocabulary</a>")
+course_name_to_info = {
+	'1': {
 		'bgcolor': "#efefef",
 		'text': "#282828",
 		'link': "#414141",
 		'vlink': "#353535",
 		'alink': "#ffaaff",
 	},
-	2: {
+	'2': {
 		'bgcolor': "#5028c9",
 		'text': "#ffff00",
-		'link': "#ffffff",
-		'vlink': "#ffffff",
-		'alink': "#ffffff",
+		'link': "white",
+		'vlink': "white",
+		'alink': "white",
 	},
-	3: {
+	'3': {
 		'bgcolor': "black",
 		'text': '#33ccff',
 		'link': 'blue',
 		'vlink': 'blue',
 		'alink': 'white',
+	},
+	'vocab': {
+		'bgcolor': "#900909",
+		'text': 'white',
+		'link': '#eee',
+		'vlink': "#ddd",
+		'alink': 'black',
 	},
 }
 # also grab content from 1-content.html and others
@@ -35,18 +43,17 @@ env = Environment(
 	autoescape=select_autoescape(['html']))
 
 # remove build dir and replace with empty build dir
-BUILD_DIR = '_build'
 rmtree(BUILD_DIR)
 mkdir(BUILD_DIR)
 
-for course_num, dic in course_num_to_info.items():
+for course_name, dic in course_name_to_info.items():
 	dic.update({
-		'course_num': course_num,
+		'course_name': course_name.capitalize(),
 		'banner_snippet': banner_snippet,
 	})
-	template = env.get_template('{}-content.html'.format(course_num))
+	template = env.get_template('{}-content.html'.format(course_name))
 	rendered_html = template.render(dic)
-	rel_output_path = '{}/{}.html'.format(BUILD_DIR, course_num)
+	rel_output_path = '{}/{}.html'.format(BUILD_DIR, course_name)
 	with open(rel_output_path, 'w') as file:
 		file.write(rendered_html)
 
